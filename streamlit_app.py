59756ee760bf0fef59fefba6e287158445d9a66b
Yajas2314 <yajasagrawal@gmail.com>
2025-05-13 12:59:00 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,9 @@ def generate_3Dmol_visualization(smiles):
     viewer.setStyle({'stick': {}})
     viewer.zoomTo()
     viewer.render()
-
-# Get the HTML content of the viewer
-viewer_html = viewer._make_html()
-return viewer_html
+    # Get the HTML content of the viewer
+    viewer_html = viewer._make_html()
+    return viewer_html
 
 # Streamlit App UI
 st.title("Molecular AR Viewer")
