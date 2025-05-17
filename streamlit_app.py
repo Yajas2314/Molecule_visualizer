4193729e4e9800c7d0d93b6ad46897d46a1a60c6
Yajas2314 <yajasagrawal@gmail.com>
2025-05-17 10:57:06 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,5 +72,4 @@ if user_input:
         viewer = draw_3d_molecule_with_lone_pairs(smiles)
         st.components.v1.html(viewer._make_html(), height=500)
 
-ply_path = generate_colored_labeled_ply("CCO")  # Ethanol
-print("Saved to:", ply_path)
+
